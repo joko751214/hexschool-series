0ccd48ce6d0248fca02800027db1886cee680392
joko751214 <joko751214@gmail.com>
2021-01-19 20:53:18 +0800

    Vue CLI

---
+++ JavaScript 入門篇 - 學徒的試煉/Code/14.Vue.js 2.0/1.Vue.js 幼幼班起手式(上)/all.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "hello world!!",
+    score: 60
+  }
+});
+
+// el => element，綁定數據要展示區域的元素
+// template 樣板
+
+// angular、react、vue，用數據來驅動內容
+
+// var message = "hello world!!" => 等同於 data 物件裡面的 key(變數)
